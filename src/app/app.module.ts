41bb4f631efbf53242aab1f01c6722cd44fa5dd7
HaruMirai <jasonvitonew@gmail.com>
2022-01-17 23:03:13 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { DivisionComponent } from './division/division.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    DivisionComponent,
+    DashboardComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/division/division.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Division } from '../model/division.model';
+import { DivisionService } from '../services/division.service';
+@Component({
+  selector: 'app-division',
+  templateUrl: './division.component.html',
+  styleUrls: ['./division.component.css']
+})
+export class DivisionComponent implements OnInit {
+  divisions?:Division[];
+  currentDivision:Division={};
+  name='';
+  constructor(private divisionService:DivisionService) { }
+
+  ngOnInit(): void {
+      this.retriveDivision()
+  }
+  retriveDivision():void{
+    this.divisionService.getAll()
+      .subscribe(
+        data=>{
+          this.divisions=data;
+          console.log(data);
+        },
+        error=>{
+          console.log(error)
+        }
+      )
+  }
+}
